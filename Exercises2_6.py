8c385a9d4029bd108b4eb774be3b089d720003f4
Thijs Markus <thijs.markus@gmail.com>
2024-04-23 17:01:36 +0200

    Fixed for even numbers

--- Exercises2_6.py
+++ Exercises2_6.py
@@ -1,7 +1,8 @@
 string_to_test = input("Please input a sentence. \n")
 string_length = len(string_to_test)
 
-if string_to_test[:string_length//2] == string_to_test[:string_length//2:-1]:
+# half the string length minus 1 works for even lengths, add a modulo 2 for uneven lengths
+if string_to_test[:string_length//2] == string_to_test[:string_length // 2 - 1 + string_length % 2:-1]:
     print("This is a palindrome")
 else:
     print("This is not a palindrome")
